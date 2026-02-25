94055b58b2c2ae3624e364ceac774ac5a1e2375a
rupeshrai05 <147249134+rupeshrai05@users.noreply.github.com>
2026-02-25 14:49:47 +0530

    Time: 11 ms (88.56%), Space: 27.8 MB (70.95%) - LeetHub

---
+++ 0218-the-skyline-problem/0218-the-skyline-problem.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        vector<pair<int,int>> events;
+        for (auto &b : buildings) {
+            events.push_back({b[0], -b[2]}); // start
+            events.push_back({b[1],  b[2]}); // end
+        }
+
+        sort(events.begin(), events.end());
+
+        multiset<int> heights;
+        heights.insert(0);
+        int prevMax = 0;
+
+        vector<vector<int>> ans;
+
+        for (auto &e : events) {
+            int x = e.first;
+            int h = e.second;
+
+            if (h < 0)
+                heights.insert(-h);
+            else
+                heights.erase(heights.find(h));
+
+            int currMax = *heights.rbegin();
+            if (currMax != prevMax) {
+                ans.push_back({x, currMax});
+                prevMax = currMax;
+            }
+        }
+
+        return ans;
+    }
+};
